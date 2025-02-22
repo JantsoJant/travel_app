ad36c1b59d2dcef6d1e183d2c89b35b0dfc452f3
JantsoJant <sa26122612@gmail.com>
2025-02-22 11:05:01 +0500

    Update sign_in.dart

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -13,35 +13,59 @@ class _SignInScreenState extends State<SignInScreen>{
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        leading: Container(
-          width: 10,
-          height: 10,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: Colors.red,
-          ),
-          child: IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.arrow_back),
-          ),
-        ),
-      ),
       body: Container(
         color: Colors.greenAccent,
         width: screenWidth,
         height: screenHeight,
         padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              width: 10,
-              height: 10,
+              width: 40,
+              height: 40,
               decoration: BoxDecoration(
+                color: Colors.grey,
                 borderRadius: BorderRadius.circular(100),
               ),
+              child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back,size: 20,))
+            ),
+            SizedBox(height: 20,),
+            Container(
+              color: Colors.white,
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      "Sign in now",
+                      style: TextStyle(
+                        fontFamily: "SUDisplay",
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700,
+                        height: 34/26,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                    Text(
+                      "Please sign in to continue our app",
+                      style: TextStyle(
+                        color: Color(0xFF7D848D),
+                        fontFamily: "SUDisplay",
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        height: 20/16,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Hello",
 
+                      )
+                    )
+                  ],
+                )
+              ),
             )
           ],
         ),
